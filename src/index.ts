feceafc075a3ab32075d1c5b171b8f9ced8e021b
Patrick Lee <cluster000@gmail.com>
2020-02-12 23:42:56 +0800

    Add json server and the fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-
-user.on('change', () => {
-  console.log('Change #2');
-});
-
-user.on('save', () => {
-  console.log('Save was triggered');
-});
-
-user.trigger('save');
-
-console.log(user);
+user.fetch();
